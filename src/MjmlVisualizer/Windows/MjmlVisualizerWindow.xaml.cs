4924d2d3b1fcdcb614bf7c7045669d1b92f55d7b
Samir Boulema <samir@auto.nl>
2023-08-03 14:10:24 +0200

    Add Save buttons

--- src/MjmlVisualizer/Windows/MjmlVisualizerWindow.xaml.cs
+++ src/MjmlVisualizer/Windows/MjmlVisualizerWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MjmlVisualizer.ViewModels;
+using System.IO;
 using System.Windows;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
 
 namespace MjmlVisualizer.Windows
 {
@@ -17,5 +19,41 @@ namespace MjmlVisualizer.Windows
 
         private void OnCloseButtonClick(object sender, RoutedEventArgs e)
             => Close();
+
+        private void OnMjmlSaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            var safeFileDialog = new SaveFileDialog
+            {
+                Filter = "MJML Template|*.mjml",
+                Title = "Save a MJML Template"
+            };
+
+            safeFileDialog.ShowDialog();
+
+            if (string.IsNullOrEmpty(safeFileDialog.FileName))
+            {
+                return;
+            }
+
+            File.WriteAllText(safeFileDialog.FileName, (DataContext as MjmlVisualizerViewModel).MJML);
+        }
+
+        private void OnHtmlSaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            var safeFileDialog = new SaveFileDialog
+            {
+                Filter = "HTML Webpage|*.html",
+                Title = "Save a HTML Webpage"
+            };
+
+            safeFileDialog.ShowDialog();
+
+            if (string.IsNullOrEmpty(safeFileDialog.FileName))
+            {
+                return;
+            }
+
+            File.WriteAllText(safeFileDialog.FileName, (DataContext as MjmlVisualizerViewModel).HTML);
+        }
     }
 }
